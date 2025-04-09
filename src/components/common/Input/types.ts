2ecd39108c9c63fbec8c504f67460422e08d710c
tomasrodriguez1 <tomas.rodriguez1@uc.cl>
2025-04-09 15:21:23 -0400

    build fix

--- src/components/common/Input/types.ts
+++ src/components/common/Input/types.ts
@@ -4,11 +4,15 @@ export interface BaseInputProps extends React.InputHTMLAttributes<HTMLInputEleme
   required?: boolean;
 }
 
-export interface EmailInputProps extends Omit<BaseInputProps, 'type'> {}
+export interface EmailInputProps extends Omit<BaseInputProps, 'type'> {
+  placeholder?: string;
+}
 
 export interface PasswordInputProps extends Omit<BaseInputProps, 'type'> {
   showPasswordRequirements?: boolean;
   onPasswordFocus?: () => void;
 }
 
-export interface TextInputProps extends Omit<BaseInputProps, 'type'> {} 
+export interface TextInputProps extends Omit<BaseInputProps, 'type'> {
+  placeholder?: string;
+} 
